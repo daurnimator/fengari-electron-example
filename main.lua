1aea9653fbb88de6fa6dc5296c7e5e716831b7aa
daurnimator <quae@daurnimator.com>
2018-01-09 00:45:11 +1100

    main.lua: Set autoHideMenuBar
    Introduces 'Object' helper. See https://gist.github.com/daurnimator/5a7fa933e96e14333962093322e0ff95

--- main.lua
+++ main.lua
@@ -1,7 +1,20 @@
 local js = require "js"
 local electron = require "electron"
 
+-- Helper to copy lua table to a new JavaScript Object
+-- e.g. Object{mykey="myvalue"}
+local function Object(t)
+	local o = js.new(js.global.Object)
+	for k, v in pairs(t) do
+		assert(type(k) == "string" or js.typeof(k) == "symbol", "JavaScript only has string and symbol keys")
+		o[k] = v
+	end
+	return o
+end
+
 electron.app:on("ready", function()
-	local win = js.new(electron.BrowserWindow)
+	local win = js.new(electron.BrowserWindow, Object{
+		autoHideMenuBar = true;
+	})
 	win:loadURL("https://fengari.io")
 end)

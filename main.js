88cc6d9279bed8eb05ccdc7aa73469d1e070b686
daurnimator <quae@daurnimator.com>
2018-02-02 20:32:00 -0800

    Update to new fengari api

--- main.js
+++ main.js
@@ -2,39 +2,68 @@
 
 const path = require("path");
 
-const {lua, lauxlib, lualib} = require('fengari');
-const interop = require('fengari-interop');
+const {
+	FENGARI_COPYRIGHT,
+	to_luastring,
+	lua: {
+		LUA_ERRSYNTAX,
+		LUA_OK,
+		LUA_REGISTRYINDEX,
+		lua_pcall,
+		lua_pop,
+		lua_pushstring,
+		lua_setfield,
+		lua_setglobal,
+		lua_tojsstring
+	},
+	lauxlib: {
+		LUA_LOADED_TABLE,
+		luaL_checkstack,
+		luaL_getsubtable,
+		luaL_loadfile,
+		luaL_newstate,
+		luaL_requiref
+	},
+	lualib: {
+		luaL_openlibs
+	}
+} = require('fengari');
+const {
+	luaopen_js,
+	push,
+	tojs
+} = require('fengari-interop');
 
-const L = lauxlib.luaL_newstate();
+const L = luaL_newstate();
 
 /* open standard libraries */
-lualib.luaL_openlibs(L);
+luaL_openlibs(L);
 /* js lib */
-lauxlib.luaL_requiref(L, lua.to_luastring("js"), interop.luaopen_js, 0);
-lua.lua_pop(L, 1); /* remove lib */
+luaL_requiref(L, to_luastring("js"), luaopen_js, 0);
+lua_pop(L, 1); /* remove lib */
 /* register electron lib */
-lauxlib.luaL_getsubtable(L, lua.LUA_REGISTRYINDEX, lauxlib.LUA_LOADED_TABLE);
-interop.push(L, require("electron"));
-lua.lua_setfield(L, -2, lua.to_luastring("electron"));  /* LOADED[modname] = module */
-lua.lua_pop(L, 1); /* remove LOADED */
+luaL_getsubtable(L, LUA_REGISTRYINDEX, LUA_LOADED_TABLE);
+push(L, require("electron"));
+lua_setfield(L, -2, to_luastring("electron"));  /* LOADED[modname] = module */
+lua_pop(L, 1); /* remove LOADED */
 
-lua.lua_pushstring(L, lua.to_luastring(lua.FENGARI_COPYRIGHT));
-lua.lua_setglobal(L, lua.to_luastring("_COPYRIGHT"));
+lua_pushstring(L, to_luastring(FENGARI_COPYRIGHT));
+lua_setglobal(L, to_luastring("_COPYRIGHT"));
 
-let ok = lauxlib.luaL_loadfile(L, lua.to_luastring(path.join(__dirname, "main.lua")));
-if (ok === lua.LUA_ERRSYNTAX) {
-	throw new SyntaxError(lua.lua_tojsstring(L, -1));
+let ok = luaL_loadfile(L, to_luastring(path.join(__dirname, "main.lua")));
+if (ok === LUA_ERRSYNTAX) {
+	throw new SyntaxError(lua_tojsstring(L, -1));
 }
-if (ok === lua.LUA_OK) {
+if (ok === LUA_OK) {
 	/* Push process.argv[2:] */
-	lauxlib.luaL_checkstack(L, process.argv.length-2, null);
+	luaL_checkstack(L, process.argv.length-2, null);
 	for (let i=2; i<process.argv.length; i++) {
-		interop.push(L, process.argv[i]);
+		push(L, process.argv[i]);
 	}
-	ok = lua.lua_pcall(L, process.argv.length-2, 0, 0);
+	ok = lua_pcall(L, process.argv.length-2, 0, 0);
 }
-if (ok !== lua.LUA_OK) {
-	let e = interop.tojs(L, -1);
-	lua.lua_pop(L, 1);
+if (ok !== LUA_OK) {
+	let e = tojs(L, -1);
+	lua_pop(L, 1);
 	throw e;
 }
